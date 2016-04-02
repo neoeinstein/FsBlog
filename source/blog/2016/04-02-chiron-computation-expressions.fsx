d1f5cddca745c652d47f737ab8d026a58011ab35
Marcus Griep <marcus@griep.us>
2016-04-02 15:11:45 -0400

    Better `do!` and `let!` explanations

--- source/blog/2016/04-02-chiron-computation-expressions.fsx
+++ source/blog/2016/04-02-chiron-computation-expressions.fsx
@@ -23,7 +23,7 @@ back from that precipice and talk about the `json{}` computation expression.
 open Chiron
 (**
 In general, computation expressions are a bit of syntactic sugar that F# provides. Internally, the
-monadic functions of `Bind` and `Return` are utilized, but that is generally transparent to the
+monadic functions of `Bind` and `Return` are used, but that is generally transparent to the
 user of a computation expression.
 
 Let's dive straight into an example from the first article:
@@ -63,6 +63,36 @@ Compared to [the previous example][Chiron1User], I think this is way more unders
 There are no custom operators to figure out, and you don't need to open `Chiron.Operators` to use it.
 
   [Chiron1User]:/blog/2015/12-13-chiron-json-ducks-monads/index.html#user-type
+
+So, what do the various parts mean? What's that `let!` and `do!`? For a fuller explanation of computation
+expressions in general, I'll point you to the excellent introduction by Scott Wlaschin on [fsharpforfunandprofit.com][FSfFaPCompExp].
+Nonetheless, let's briefly break this down.
+
+  [FSfFaPCompExp]:https://fsharpforfunandprofit.com/posts/computation-expressions-intro/
+*)
+(*** do-not-eval ***)
+    let! n = Json.read "name"
+(**
+The first line that we will look at is from the `FromJson` function. In this case, we are reading the `name` member
+from the hidden `Json` object. This deserialization may or may not be successful. Presuming that it is successful, we
+want the value to be bound to `n`, but if it fails, we want to short-circuit and report the error.
+
+The key to making this work is the `!` in `let!`. The `!` provides a signal to the F# compiler that what we really want
+is the `string` from the `Json<string>` returned by `Json.read`. Chiron's `json{}` implementation of provides such a
+`bind` function which takes care of unwrapping the deserialized value, or in case deserialization fails, triggers the
+short-circuit to return the deserialization error.
+*)
+(*** do-not-eval ***)
+    do! Json.write "name" x.Name
+(**
+Things are subtly different in the `ToJson` function. In this case, we are updating the hidden `Json` object being
+held by the computation expression. The write doesn't return anything meaningful but it does have a return type of
+`Json<unit>`. The above is syntactically equivalent to:
+*)
+(*** do-not-eval ***)
+    let! _ = Json.write "name" x.Name
+(**
+The `do!` just plain looks better than binding to an ignored value.
 
 In the long-promised [next article][Chiron3], we'll finally delve into dealing with missing data and union types.
 
